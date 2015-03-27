1a68d2c7e970d884bcd7eadc4995fe5ae2e25097
nickdujay@gmail.com <dujaynic@iits-b473-20049.(none)>
2015-03-27 13:57:38 -0400

    add final stuff?

--- a5/programming/basic.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-./runner.py -s 1234 -u 0 -f 0 -r 1.0 -t 0.05
--- a5/programming/runner.sh
+++ a5/programming/runner.sh
@@ -1,7 +1,3 @@
 #!/bin/zsh
 
-./runner.py --seed 8234 --units 20 --function 1 --rate 0.5 --threshold 0.05
-
-# units: 20, rate: 1.0, starting 0.18 -> 0.11
-# units: 17, rate: 2.0, starting 0.28 -> 0.38
-# units: 20, rate: 0.5, starting 0.15 -> 0.08
+./runner.py --seed 9244 --units 15 --function 0 --rate 0.4 --threshold 0.05
